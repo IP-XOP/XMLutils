b2b77839b7c22b61c60120b1b84a3f7225ff6359
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2009-10-15 08:25:47 +0000

    checking in new version

--- src/XMLfileIO.cpp
+++ src/XMLfileIO.cpp
@@ -12,7 +12,7 @@
 //a utility that converts Mac paths to UNIX paths
 #ifdef _MACINTOSH_
 int ConvertPath(const char * inPath, char * outPath, int outPathMaxLen) {
-
+	
 	CFStringRef inStr = CFStringCreateWithCString(kCFAllocatorDefault, inPath ,kCFStringEncodingMacRoman);
 	if (inStr == NULL)
 		return -1;
@@ -40,8 +40,10 @@ XMLopenFile(XMLopenFileStruct *p){
 	
 	igorXMLfile openfile;
 	xmlDoc *doc = NULL;
-//	xmlValidCtxt *ctxt = NULL;
+	//	xmlValidCtxt *ctxt = NULL;
 	extern std::map<long,igorXMLfile> allXMLfiles;	
+	FILE *fp = NULL;
+	
 	
 	if(err = GetCStringFromHandle(p->fullFilePath,fullFilePath,MAX_PATH_LEN))
 		goto done;
@@ -49,36 +51,35 @@ XMLopenFile(XMLopenFileStruct *p){
 	if (err = GetNativePath(fullFilePath,nativePath))
 		goto done;
 	
-
 	//if the file doesn't point to anything return an error, this is so programmatic aborts are less likely to happen
-//	if(FullPathPointsToFile(nativePath) == 0){
-//		err = 0;
-//		p->retval = -2;
-//		XOPNotice("XMLopenfile: File(path) to open doesn't exist\r");
-//		goto done;
-//	}
+	//	if(FullPathPointsToFile(nativePath) == 0){
+	//		err = 0;
+	//		p->retval = -2;
+	//		XOPNotice("XMLopenfile: File(path) to open doesn't exist\r");
+	//		goto done;
+	//	}
 	
 	//convert native Mac path to UNIX
-	#ifdef _MACINTOSH_
+#ifdef _MACINTOSH_
 	//see if its a MAC path by seeing if there is the Mac delimiter : in there
 	if((isMAC = strstr(nativePath,macdelim)) && (err = HFSToPosixPath(nativePath,unixpath,0)))
 		goto done;
 	if(isMAC)
 		strcpy(nativePath, unixpath);
-	#endif
+#endif
 	
-	FILE *fp = fopen(nativePath,"r");
+	fp = fopen(nativePath,"r");
 	if( fp ) {
-	// exists
+		// exists
 		fclose(fp);
 	} else {
 		err = 0;
 		p->retval = -2;
 		XOPNotice("XMLopenfile: File(path) to open doesn't exist, or file can't be opened\r");
 		goto done;
-	// doesnt exist, at least not for reading
+		// doesnt exist, at least not for reading
 	}
-		
+	
 	/* Load XML document */
     doc = xmlParseFile(nativePath);
     if (doc == NULL) {
@@ -89,19 +90,19 @@ XMLopenFile(XMLopenFileStruct *p){
     }
 	
 	//validate the document
-//	ctxt = xmlNewValidCtxt();
-//	if (ctxt == NULL) {
-//		err = XML_PARSERCTXT_ERROR;
-//		goto done;
-//	}
-//	if(xmlValidateDocument(ctxt,doc) != 1){
-//		err = XMLDOC_NOTVALIDATED;
-//		goto done;
-//	}
-//	if(xmlValidateDocumentFinal(ctxt,doc) != 1){
-//		err = XMLDOC_NOTVALIDATED;
-//		goto done;
-//	}
+	//	ctxt = xmlNewValidCtxt();
+	//	if (ctxt == NULL) {
+	//		err = XML_PARSERCTXT_ERROR;
+	//		goto done;
+	//	}
+	//	if(xmlValidateDocument(ctxt,doc) != 1){
+	//		err = XMLDOC_NOTVALIDATED;
+	//		goto done;
+	//	}
+	//	if(xmlValidateDocumentFinal(ctxt,doc) != 1){
+	//		err = XMLDOC_NOTVALIDATED;
+	//		goto done;
+	//	}
 	
 	
 	if(err = XOPOpenFile(nativePath,0,&fileRef)){
@@ -115,8 +116,7 @@ XMLopenFile(XMLopenFileStruct *p){
 	allXMLfiles[fileno(fileRef)] = openfile;
 	p->retval = fileno(fileRef);
 	
-	
-	done:
+done:
 	if(err)
 		if(doc!= NULL)
 			xmlFreeDoc(doc);
@@ -127,8 +127,8 @@ XMLopenFile(XMLopenFileStruct *p){
 	if(err)
 		p->retval = -2;
 	
-//	if(ctxt != NULL)
-//		xmlFreeValidCtxt(ctxt);
+	//	if(ctxt != NULL)
+	//		xmlFreeValidCtxt(ctxt);
 	if(p->fullFilePath)
 		DisposeHandle(p->fullFilePath);
 	return err;
@@ -143,7 +143,7 @@ XMLcloseFile(XMLcloseFileStruct *p){
 	extern std::map<long,igorXMLfile> allXMLfiles;
 	std::map<long,igorXMLfile>::iterator allXMLfiles_iter;
 	igorXMLfile tmp;
-		
+	
 	fileID = (long)(roundf(p->fileID));
 	
 	xmlIndentTreeOutput = 1;
@@ -155,10 +155,10 @@ XMLcloseFile(XMLcloseFileStruct *p){
 				
 				if(p->toSave){
 					rewind(allXMLfiles[fileID].fileRef);
-//					if(xmlDocFormatDump(tmp.fileRef,tmp.doc,1) == -1){
-//						err = XML_COULDNT_SAVE;
-//						goto done;
-//					}
+					//					if(xmlDocFormatDump(tmp.fileRef,tmp.doc,1) == -1){
+					//						err = XML_COULDNT_SAVE;
+					//						goto done;
+					//					}
 					
 					if(err = XOPCloseFile(tmp.fileRef))
 						goto done;
@@ -182,13 +182,13 @@ XMLcloseFile(XMLcloseFileStruct *p){
 			if(allXMLfiles.find(fileID) != allXMLfiles.end()){
 				if(p->toSave){
 					tmp = allXMLfiles[fileID];
-
+					
 					rewind(allXMLfiles[fileID].fileRef);
-//					if(xmlDocFormatDump(tmp.fileRef,tmp.doc,0) == -1){
-//						err = XML_COULDNT_SAVE;
-//						goto done;
-//					}
-
+					//					if(xmlDocFormatDump(tmp.fileRef,tmp.doc,0) == -1){
+					//						err = XML_COULDNT_SAVE;
+					//						goto done;
+					//					}
+					
 					if(err = XOPCloseFile((allXMLfiles[fileID].fileRef)))
 						goto done;
 					if(xmlSaveFormatFileEnc(tmp.fileNameStr , tmp.doc , NULL , 1) == -1){
@@ -204,10 +204,10 @@ XMLcloseFile(XMLcloseFileStruct *p){
 					allXMLfiles.erase(fileID);
 				}
 			}
-		}
+	}
 done:
 	(err == 0)? (p->retval = 0):(p->retval = -1);
-
+	
 	return err;
 }
 
@@ -230,17 +230,17 @@ XMLSAVEFILE(XMLfileSaveStruct *p){
 		doc = (allXMLfiles[p->fileID].doc);
 		fileName = (char*) allXMLfiles[p->fileID].fileNameStr;
 	}
-
+	
 	xmlIndentTreeOutput = 1;
 	
 	rewind(allXMLfiles[fileID].fileRef);
-//	if(xmlDocFormatDump(allXMLfiles[fileID].fileRef,allXMLfiles[fileID].doc,1) == -1){
-//		err = XML_COULDNT_SAVE;
-//		goto done;
-//	}
-
+	//	if(xmlDocFormatDump(allXMLfiles[fileID].fileRef,allXMLfiles[fileID].doc,1) == -1){
+	//		err = XML_COULDNT_SAVE;
+	//		goto done;
+	//	}
+	
 	if(xmlSaveFormatFileEnc(allXMLfiles[fileID].fileNameStr , doc , NULL , 1) == -1){
-//	if(xmlSaveFile(allXMLfiles[p->fileID].fileNameStr , doc) == -1){
+		//	if(xmlSaveFile(allXMLfiles[p->fileID].fileNameStr , doc) == -1){
 		err = XML_COULDNT_SAVE;
 		goto done;
 	}
@@ -252,128 +252,128 @@ done:
 
 int
 XMLcreateFile(XMLcreateFileStruct *p){
-int err = 0;
-
-XOP_FILE_REF fileRef = NULL;
-char fullFilePath [MAX_PATH_LEN+1], nativePath[MAX_PATH_LEN+1],unixpath[MAX_PATH_LEN+1];
-char *macdelim = ":";
-char *isMAC = NULL;
-
-igorXMLfile openfile;
-xmlDoc *doc = NULL;
-xmlNode *root_element= NULL ;
-xmlNs *nspace = NULL;
-char *rootname   = NULL;
-char *ns = NULL;
-char *prefix = NULL;
-
-extern std::map<long,igorXMLfile> allXMLfiles;
-
-p->fileID = -1;
-
-if(p->rootelement == NULL || p->ns == NULL){
-	err = NULL_STRING_HANDLE;
-	goto done;
-}
-
-//allocate space for the C-strings.
-rootname = (char*)malloc((GetHandleSize(p->rootelement)+1)*sizeof(char));
-if(rootname == NULL){
-	err = NOMEM;goto done;
-}
-ns = (char*)malloc((GetHandleSize(p->ns)+1)*sizeof(char));
-if(ns == NULL){
-	err = NOMEM;goto done;
-}
-prefix = (char*)malloc((GetHandleSize(p->prefix)+1)*sizeof(char));
-if(prefix == NULL){
-	err = NOMEM;goto done;
-}
-
-/* get all of the igor input strings into C-strings */
-if (err = GetCStringFromHandle(p->rootelement,rootname,GetHandleSize(p->rootelement)))
-	goto done;
-if (err = GetCStringFromHandle(p->ns, ns, GetHandleSize(p->ns)))
-	goto done;
-if (err = GetCStringFromHandle(p->prefix, prefix, GetHandleSize(p->prefix)))
-	goto done;
-if(err = GetCStringFromHandle(p->fileName,fullFilePath,MAX_PATH_LEN))
-	goto done;
-//get native filesystem filepath
-if (err = GetNativePath(fullFilePath,nativePath))
-	goto done;
+	int err = 0;
 	
-//convert native Mac path to UNIX
-#ifdef _MACINTOSH_
-//see if its a MAC path by seeing if there is the Mac delimiter : in there
-if((isMAC = strstr(nativePath,macdelim)) && (err = HFSToPosixPath(nativePath,unixpath,0)))
-	goto done;
-if(isMAC)
-	strcpy(nativePath, unixpath);
-#endif
-
-//create a new XML document
-doc = xmlNewDoc(BAD_CAST "1.0");
-if(doc == NULL){
-	err = COULDNT_CREATE_XMLDOC;
-	goto done;
-}
-
-//check if the node name is invalid
-if(xmlValidateName(BAD_CAST rootname , 0) != 0){
-	err = INVALID_NODE_NAME;
-	goto done;
-}
-		
-
-//create the root element
-root_element=xmlNewNode(NULL , BAD_CAST rootname);
-if(root_element == NULL){
-	err = COULDNT_CREATE_NODE;
-	goto done;
-}
-
-if(strlen(prefix)>0){
-	nspace = xmlNewNs(root_element, BAD_CAST ns, BAD_CAST prefix );
-	root_element->ns = nspace;
-} else {
-	nspace = xmlNewNs(root_element, BAD_CAST ns, NULL );
-}
-
-root_element = xmlDocSetRootElement(doc,root_element);
-root_element = xmlDocGetRootElement(doc);
-if(root_element == NULL){
-	err = COULDNT_CREATE_NODE;
-	goto done;
-}
-
-if(err = XOPOpenFile(nativePath,1,&fileRef))
-	goto done;
-
-strcpy(openfile.fileNameStr,nativePath);
-openfile.doc = doc;
-openfile.fileRef = fileRef;
-
-allXMLfiles[fileno(fileRef)] = openfile;
-p->fileID = fileno(fileRef);
-
-done:		
-if(err){
+	XOP_FILE_REF fileRef = NULL;
+	char fullFilePath [MAX_PATH_LEN+1], nativePath[MAX_PATH_LEN+1],unixpath[MAX_PATH_LEN+1];
+	char *macdelim = ":";
+	char *isMAC = NULL;
+	
+	igorXMLfile openfile;
+	xmlDoc *doc = NULL;
+	xmlNode *root_element= NULL ;
+	xmlNs *nspace = NULL;
+	char *rootname   = NULL;
+	char *ns = NULL;
+	char *prefix = NULL;
+	
+	extern std::map<long,igorXMLfile> allXMLfiles;
+	
 	p->fileID = -1;
-	if(doc != NULL)
-		xmlFreeDoc(doc);
-	if(nspace != NULL)
-		xmlFreeNs(nspace);
-}
-if(rootname != NULL)
-	free(rootname);
-if(ns != NULL)
-	free(ns);
-if(prefix != NULL)
-	free(prefix);
-DisposeHandle(p->prefix);
-DisposeHandle(p->fileName);
-DisposeHandle(p->rootelement);
-DisposeHandle(p->ns);
-return err;
+	
+	if(p->rootelement == NULL || p->ns == NULL){
+		err = NULL_STRING_HANDLE;
+		goto done;
+	}
+	
+	//allocate space for the C-strings.
+	rootname = (char*)malloc((GetHandleSize(p->rootelement)+1)*sizeof(char));
+	if(rootname == NULL){
+		err = NOMEM;goto done;
+	}
+	ns = (char*)malloc((GetHandleSize(p->ns)+1)*sizeof(char));
+	if(ns == NULL){
+		err = NOMEM;goto done;
+	}
+	prefix = (char*)malloc((GetHandleSize(p->prefix)+1)*sizeof(char));
+	if(prefix == NULL){
+		err = NOMEM;goto done;
+	}
+	
+	/* get all of the igor input strings into C-strings */
+	if (err = GetCStringFromHandle(p->rootelement,rootname,GetHandleSize(p->rootelement)))
+		goto done;
+	if (err = GetCStringFromHandle(p->ns, ns, GetHandleSize(p->ns)))
+		goto done;
+	if (err = GetCStringFromHandle(p->prefix, prefix, GetHandleSize(p->prefix)))
+		goto done;
+	if(err = GetCStringFromHandle(p->fileName,fullFilePath,MAX_PATH_LEN))
+		goto done;
+	//get native filesystem filepath
+	if (err = GetNativePath(fullFilePath,nativePath))
+		goto done;
+	
+	//convert native Mac path to UNIX
+#ifdef _MACINTOSH_
+	//see if its a MAC path by seeing if there is the Mac delimiter : in there
+	if((isMAC = strstr(nativePath,macdelim)) && (err = HFSToPosixPath(nativePath,unixpath,0)))
+		goto done;
+	if(isMAC)
+		strcpy(nativePath, unixpath);
+#endif
+	
+	//create a new XML document
+	doc = xmlNewDoc(BAD_CAST "1.0");
+	if(doc == NULL){
+		err = COULDNT_CREATE_XMLDOC;
+		goto done;
+	}
+	
+	//check if the node name is invalid
+	if(xmlValidateName(BAD_CAST rootname , 0) != 0){
+		err = INVALID_NODE_NAME;
+		goto done;
+	}
+	
+	
+	//create the root element
+	root_element=xmlNewNode(NULL , BAD_CAST rootname);
+	if(root_element == NULL){
+		err = COULDNT_CREATE_NODE;
+		goto done;
+	}
+	
+	if(strlen(prefix)>0){
+		nspace = xmlNewNs(root_element, BAD_CAST ns, BAD_CAST prefix );
+		root_element->ns = nspace;
+	} else {
+		nspace = xmlNewNs(root_element, BAD_CAST ns, NULL );
+	}
+	
+	root_element = xmlDocSetRootElement(doc,root_element);
+	root_element = xmlDocGetRootElement(doc);
+	if(root_element == NULL){
+		err = COULDNT_CREATE_NODE;
+		goto done;
+	}
+	
+	if(err = XOPOpenFile(nativePath,1,&fileRef))
+		goto done;
+	
+	strcpy(openfile.fileNameStr,nativePath);
+	openfile.doc = doc;
+	openfile.fileRef = fileRef;
+	
+	allXMLfiles[fileno(fileRef)] = openfile;
+	p->fileID = fileno(fileRef);
+	
+done:		
+	if(err){
+		p->fileID = -1;
+		if(doc != NULL)
+			xmlFreeDoc(doc);
+		if(nspace != NULL)
+			xmlFreeNs(nspace);
+	}
+	if(rootname != NULL)
+		free(rootname);
+	if(ns != NULL)
+		free(ns);
+	if(prefix != NULL)
+		free(prefix);
+	DisposeHandle(p->prefix);
+	DisposeHandle(p->fileName);
+	DisposeHandle(p->rootelement);
+	DisposeHandle(p->ns);
+	return err;
 }
