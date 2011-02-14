828d64e40a9e849572146d8233e28ed4dbffec3a
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2011-02-14 08:25:06 +0000

    added stringtokenizer

---
+++ src/StringTokenizer.h
@@ -0,0 +1,15 @@
+/*
+ *  StringTokenizer.h
+ *  iPeek
+ *
+ *  Created by andrew on 18/05/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <vector>
+#include <string>
+using namespace std;
+
+/* in StringTokenizer.cpp */
+void Tokenize(const unsigned char *, size_t, vector<string>&,  const char*,int);
